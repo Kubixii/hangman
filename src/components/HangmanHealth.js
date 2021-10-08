64625767f09795db3615730772ebde9a2578598c
Kubixii <39913230+Kubixii@users.noreply.github.com>
2021-10-08 13:44:50 +0200

    changed thing

--- src/components/HangmanHealth.js
+++ src/components/HangmanHealth.js
@@ -5,7 +5,7 @@ const HangmanHealth = props => {
     let image = ["blank.png", "0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "6.png"];
     return (
         <>
-            <img src={`./${image[faults]}`} alt="health" />
+            <img src={`/${image[faults]}`} alt="health" />
         </>
     )
 }
