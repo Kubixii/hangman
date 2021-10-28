b277d90c22007d2222bf38537eed7341c514bafd
Kubixii <39913230+Kubixii@users.noreply.github.com>
2021-10-28 20:50:27 +0200

    Updates

---
+++ precache-manifest.b8bc512425df6a9a4b6c9d0ca89057bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c85b7dcf300747fc5cfd",
+    "url": "/hangman/static/css/main.ca138a83.chunk.css"
+  },
+  {
+    "revision": "c85b7dcf300747fc5cfd",
+    "url": "/hangman/static/js/main.31aa3023.chunk.js"
+  },
+  {
+    "revision": "19ffbe7598942725a8e1",
+    "url": "/hangman/static/js/runtime~main.d9fce119.js"
+  },
+  {
+    "revision": "20128b846ae410257bfc",
+    "url": "/hangman/static/js/2.76ba818f.chunk.js"
+  },
+  {
+    "revision": "0262910a7d27f1a9a002737aea899e16",
+    "url": "/hangman/index.html"
+  }
+];
